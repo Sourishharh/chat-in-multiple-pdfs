9725a5d7aa6ca8a391d88cbb878462fa7cedd4d0
Sourishharh <sourishharh@gmail.com>
2025-01-29 23:10:13 +0530

    all changes

--- Start.py
+++ Start.py
@@ -4,12 +4,11 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
-from faiss import IndexFlatL2
 
 
 # Load environment variables 
